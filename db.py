5eaaa0bc5cac1e599fc32c2e107e878c0714bf08
Emberyn <2855735504@qq.com>
2026-03-21 11:55:18 +0800

    修复一些问题
    1.教师端评价作业失败
    2.
    在学生端
    左边的列表本来是仪表盘、我的作业、我的成绩、学习计划
    但点击“我的作业”之后
    左边的列表变成了仪表盘，任务中心（作业），成绩查询
    同样的，点击“我的成绩”和“学习计划”之后也有这个问题
    即左侧导航栏的菜单名称在不同页面不一致

--- db.py
+++ db.py
@@ -1,11 +1,16 @@
+import os
 import pymysql
 import pymysql.cursors
+from dotenv import load_dotenv
+
+# 加载 .env 文件中的环境变量
+load_dotenv()
 
 DB_CONFIG = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': '123456',  # 你的密码
-    'database': 'online_learning_db',
+    'host': os.getenv('DB_HOST', 'localhost'),
+    'user': os.getenv('DB_USER', 'root'),
+    'password': os.getenv('DB_PASSWORD', '123456'),  # 如果环境变量没配置，默认回退到 123456
+    'database': os.getenv('DB_NAME', 'online_learning_db'),
     'charset': 'utf8mb4',
     'cursorclass': pymysql.cursors.DictCursor
 }
